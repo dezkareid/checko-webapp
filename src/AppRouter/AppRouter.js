8ff256d29d1e30bb8831f0edef1feb32f491c471
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-09 20:39:47 -0500

    added event component to router

--- src/AppRouter/AppRouter.js
+++ src/AppRouter/AppRouter.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
+import EventPage from '../pages/EventPage/EventPage'
 export function AppRouter (props) {
   return (
     <Router>
       <Route path='/' exact component={HomePage} />
+      <Route path='/events/:id' component={EventPage} />
     </Router>
   )
 }
