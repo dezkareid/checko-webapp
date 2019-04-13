17f6bc7fb3b5fdc5a0226cbc1f0001e6bd9e931b
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-12 20:30:01 -0500

    added load of event

--- src/pages/EventPage/EventPage.js
+++ src/pages/EventPage/EventPage.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Page from '../../components/Page/Page'
+import { getEvent } from '../../services/ApiService'
 
-export function EventPage (props) {
-  return (<div>EventPage</div>)
+export class EventPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+  }
+
+  componentDidMount () {
+    const { params: { id } } = this.props.match
+    getEvent(id)
+      .then(event => this.setState(event))
+  }
+  render () {
+    return (
+      <Page title={`Evento - ${this.state.name}`}>
+        <h1>{this.state.name}</h1>
+      </Page>
+    )
+  }
 }
 
 export default EventPage
