32b2b9328d4bc6aff0afc230d2eb48e11c2e0baa
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-15 20:57:34 -0500

    added route to register users

--- src/AppRouter/AppRouter.js
+++ src/AppRouter/AppRouter.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import EventPage from '../pages/EventPage/EventPage'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
 export function AppRouter (props) {
   return (
     <Router>
       <Route path='/' exact component={HomePage} />
       <Route path='/events/:id' component={EventPage} />
+      <Route path='/register' component={RegisterPage} />
     </Router>
   )
 }
