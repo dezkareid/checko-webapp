6f130599117a68230ae69e7227645a5a23a01208
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-15 20:55:49 -0500

    added method to register user

--- src/services/ApiService/index.js
+++ src/services/ApiService/index.js
@@ -11,3 +11,15 @@ export function getEvent (id) {
   return fetch(`http://localhost:9000/api/event/${id}`)
     .then(response => response.json())
 }
+
+export function registerUser (user) {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost:9000/api/user/', options)
+    .then(response => response.json())
+}
