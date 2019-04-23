c5de06280287913445cbd101cbc37bf61afcd238
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-22 20:13:03 -0500

    added loging page to router

--- src/AppRouter/AppRouter.js
+++ src/AppRouter/AppRouter.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import EventPage from '../pages/EventPage/EventPage'
+import LoginPage from '../pages/LoginPage/LoginPage'
 export function AppRouter (props) {
   return (
     <Router>
       <Route path='/' exact component={HomePage} />
       <Route path='/events/:id' component={EventPage} />
+      <Route path='/login' component={LoginPage} />
     </Router>
   )
 }
