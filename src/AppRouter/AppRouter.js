f2ad84d0dcd1f0f64b3fc8a0195350c900c81582
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-22 21:03:50 -0500

    added page not found and switch for router

--- src/AppRouter/AppRouter.js
+++ src/AppRouter/AppRouter.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import EventPage from '../pages/EventPage/EventPage'
 import LoginPage from '../pages/LoginPage/LoginPage'
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
 export function AppRouter (props) {
   return (
     <Router>
-      <Route path='/' exact component={HomePage} />
-      <Route path='/events/:id' component={EventPage} />
-      <Route path='/login' component={LoginPage} />
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/events/:id' component={EventPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </Router>
   )
 }
